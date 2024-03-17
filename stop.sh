beeecd5abfaad4246e211484d12d5d15d20ed26b
Connor Ashcroft <connorashcroft@live.com>
2024-03-16 22:30:26 -0700

    Add Glances port

--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@
 host=$(head -1 /usr/bin/mia-tunnel/mia_ip.txt)
 iptables -t nat -D PREROUTING -p tcp --dport 8096 -j DNAT --to-destination $host:8096
 iptables -t nat -D PREROUTING -p tcp --dport 8096 -j DNAT --to-destination $host:8200
+iptables -t nat -D PREROUTING -p tcp --dport 61208 -j DNAT --to-destination $host:61208
 iptables -t nat -D PREROUTING -p udp --dport 1900 -j DNAT --to-destination $host:1900
 iptables -t nat -D PREROUTING -p udp --dport 7359 -j DNAT --to-destination $host:7359
 iptables -t nat -D PREROUTING -p tcp --dport 80 -j DNAT --to-destination $host:80
